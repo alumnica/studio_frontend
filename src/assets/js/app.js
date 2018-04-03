1b341232827236b6f7e26fe7b7faeff4573b5caa
Kinich Barcelo <kinich.barcelo@code-ing.com>
2018-04-03 12:32:48 -0500

    Finish Ambitos-Edit Tag system Materia slection system

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,5 @@
+// import 'taggle';
+
 import $ from 'jquery';
 import whatInput from 'what-input';
 
@@ -7,6 +9,7 @@ window.$ = $;
 
 // import 'jquery-ui/ui/core';
 import 'jquery-ui/ui/widgets/sortable';
+import 'jquery-ui/ui/effects/effect-bounce';
 
 
 import Foundation from 'foundation-sites';
@@ -20,34 +23,36 @@ import 'datatables.net-responsive-zf';
 
 //taggle
 
-//jquery image upload
+//selectize
+
+import 'selectize';
 
 // import './lib/jquery.uploadPreview';
 
 $(document).foundation();
 
 
-    $('#tabla-ambitos').DataTable( {
-        responsive: true,
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-        },
-        "columnDefs": [
-            { "orderable": false, "targets": [2, 3, 4, 6] }
-        ],
+$('#tabla-ambitos').DataTable({
+    responsive: true,
+    "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+    },
+    "columnDefs": [
+        { "orderable": false, "targets": [2, 3, 4, 6] }
+    ],
 
-    } );  
+});
 
-    $('#tabla-materias').DataTable( {
-        responsive: true,
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-        },
-        "columnDefs": [
-            { "orderable": false, "targets": [2] }
-        ],
+$('#tabla-materias').DataTable({
+    responsive: true,
+    "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+    },
+    "columnDefs": [
+        { "orderable": false, "targets": [2] }
+    ],
 
-    } );  
+});
 
 
 
@@ -73,18 +78,118 @@ $("#fileUpload").on('change', function () {
     }
 });
 
-$('#changedtext').keyup(function() {
+$('#changedtext').keyup(function () {
     var textToChange = $(this).val();
     $('.ttc2').text(textToChange);
-  });
+});
 
-  $('#color-button input[type=radio]').click(function()  {
+$('#color-button input[type=radio]').click(function () {
     let $this = $(this);
     $('#image-holder').attr('class', $this.data('a'));
     $('#ttc').attr('class', $this.data('b'));
-  });
-  
-  $( function() {
-    $( "#sortable" ).sortable();
+});
+
+
+
+//selectize usage
+
+var ambitoTags = [
+    {name: 'tag1', value: 't1'},
+    {name: 'tag2', value: 't2'},
+    {name: 'tag3', value: 't3'},
+    {name: 'cosa', value: 'cs'},
+    {name: 'tag5', value: 't5'},
+    {name: 'OTHER', value: 'ot'},
+    {name: 'tag7', value: 't7'},
+];
+
+$('#ambito-tags').selectize({
+    labelField: 'name',
+    valueField: 'value',
+    searchField: 'name',
+    hideSelected: true,
+    persist: false,
+    createOnBlur: true,
+    create: function(input) {
+        return {
+            value: input,
+            name: input
+        }
+    },
+    options: ambitoTags,
+    preload: false,
+});
+
+var materiasList = [ 
+    {name: 'Afghanistan', code: 'AF'}, 
+    {name: 'Åland Islands', code: 'AX'}, 
+    {name: 'Albania', code: 'AL'},
+];
+
+$('#select-materias').selectize({
+    maxItems: 1,
+    labelField: 'name',
+    valueField: 'name',
+    searchField: 'name',
+    options: materiasList,
+    preload: true,
+});
+
+
+//sortable ambitos
+
+$(function () {
+    $("#sortable").sortable({
+    //     update:function(event, ui){
+    //         alert($(ui.item).text());
+    //    }
+    });
     // $( "#sortable" ).disableSelection();
-  } );
+});
+
+
+$("#sortable").on('click', '.remove_materia', function () {
+    $(this).parent().remove(); 
+});
+
+var numMateria = 1;
+
+$(".btn").click(function (e) {
+    e.preventDefault();
+    var text = $("select[name='materias-select']").val();
+    
+    // var $li = $('<li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s sorter"></span>some text<span class="remove_materia"><a href="#"><i class="fas fa-minus-square"></i></a></span></li>');
+    $("#sortable").append('<li id="mat_'+ numMateria++ +'" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s sorter"></span>' + text + '<span class="remove_materia"><a href="#"><i class="fas fa-minus-square"></i></a></span></li>');
+    $("#sortable").sortable('refresh');
+});
+
+$('form#ambito-creation').submit(function(){
+     
+    
+    // $('#class_name').val($( "#sortable" ).sortable("serialize"));
+    // $('#class_name')
+    var texts = [];
+
+    $(function() {
+        $('#sortable li').each(function(){
+            texts.push($(this).text());
+        });
+
+        // alert(texts);
+        $('#class_name').val(texts);
+    });
+
+    // 
+
+   return false;
+}); 
+
+// var texts = [];
+
+// $(function() {
+//     $('#sortable li').each(function(){
+//         texts.push($(this).text());
+//     });
+
+//     alert(texts);
+// });
