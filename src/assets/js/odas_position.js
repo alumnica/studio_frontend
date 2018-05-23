2ec32205c7bcb27de7582fd5d35001c4e9289455
Kinich Barcelo <kinich.barcelo@code-ing.com>
2018-05-22 21:09:54 -0500

    Send oda position on 'guardar'

--- src/assets/js/odas_position.js
+++ src/assets/js/odas_position.js
@@ -15,19 +15,17 @@ $(document).ready(function () {
         accept: ".block",
         drop: function (event, ui) {
             var theDrag = ui.draggable;
-
-
             theDrag.children('.return-button').remove();
-
             ui.draggable.detach().appendTo($(this));
             var blockId = ui.draggable.attr("id"),
                 blockId = blockId.split("-").pop();
-
-
             var closBtnContent = ('<div class="return-button" block-val="ob-' + blockId + '"><i class="fa fa-times"></i></div>')
 
-            ui.draggable.append(closBtnContent)
+            ui.draggable.append(closBtnContent);
             $(this).droppable('option', 'accept', ui.draggable);
+
+            var dropCoor = $(this).attr('id');
+            ui.draggable.attr('data-n', dropCoor);
         },
         out: function (event, ui) {
             $(this).droppable('option', 'accept', '.block');
@@ -45,13 +43,12 @@ $(document).ready(function () {
         // For some browsers, `attr` is undefined; for others,
         // `attr` is false.  Check for both.
         if (typeof attr !== typeof undefined && attr !== false) {
+            //return button
             var blockId = $(this).attr("id"),
                 blockId = blockId.split("-").pop();
-
             var closBtnContent = ('<div class="return-button" block-val="ob-' + blockId + '"><i class="fa fa-times"></i></div>')
-
             $(this).append(closBtnContent);
-
+            //append to section if already has data-n
             var theBlocknum = $(this).attr('data-n');
             var theBlock = $('#' + theBlocknum);
             $(this).appendTo(theBlock);
@@ -67,5 +64,23 @@ $(document).ready(function () {
 		$(this).parent().parent().droppable('option', 'accept', '.block');
         $(this).parent().appendTo('#ob-'+sendId);
         $(this).remove();
-    }); 
+    });
+    
+    $('#psBtn').on('mouseover', function(){
+        var posTotal = '';
+        $('#oda-position').val(posTotal)
+        $('.dropzone').each(function(){
+            if ( $(this).children().length > 0 ) {                
+                var blockNum = $(this).children().attr('id').split("-").pop();
+                var dropNum = $(this).children().attr('data-n');
+                var posTotal = (blockNum+','+dropNum);
+                if ($('#oda-position').val()=='') {
+                    $('#oda-position').val(posTotal)
+                } else {
+                    $('#oda-position').val(posTotal+';'+$('#oda-position').val());
+                }
+           }
+        });
+    });
+    
 });
