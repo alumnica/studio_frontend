9d4bdcad0c698165d76121117da186e18ff0574f
Kinich Barcelo <kinich.barcelo@code-ing.com>
2018-05-25 18:48:51 -0500

    Fix image preview modal & import bib.js changes from studio_webapp
    Corrected the class names for the image preview modal from the image library to be the same size on all desktop screens & full screen on all mobile devices.
    
    Copied all the bib.js changes from studio_webapp as the jplist funcitonality cannot be done on the front-end stnadalone framework. It requireres calls to the server. This should be the only file that is worked studio_webapp => vle_front-end.

--- src/assets/js/bib.js
+++ src/assets/js/bib.js
@@ -1,29 +1,165 @@
+// // <!-- Example of JavaScript options for PHP server side -->
+
+// $('document').ready(function(){
+//     var $list = $('#img-jplist .list')
+//          ,template = Handlebars.compile($('#jplist-template').html());
+
+//     $('#delete_images').on('click', function () {
+//        let urls = $('#toBeDeleted').val();
+//        let urls_array = urls.split(',');
+
+//        for (let i=0; i<urls_array.length; i++){
+//            delete_article(urls_array[i]);
+//        }
+//     });
+
+//     function delete_article(url){
+//     $.ajax({
+//         url: url,
+//         data: url,
+//         type: "DELETE",
+//         success: function(son) {
+//             console.log("delete successful")
+//         },
+//     })
+//  };
+
+//    $('#img-jplist').jplist({
+
+//       itemsBox: '.list'
+//       ,itemPath: '.img-box'
+//       ,panelPath: '.filter-panel'
+// //      ,redrawCallback: function(collection, $dataview, statuses){
+// //        fill();
+// //       }
+
+//       //data source
+//       ,dataSource: {
+
+//            type: 'server'
+//            , server: {
+
+//                //jQuery ajax settings
+//                ajax: {
+//                    url: '/api/images/'
+//                    , dataType: 'json'
+//                    , type: 'GET'
+//                    ,contentType: 'application/json'
+//                }
+
+//            }
+//            ,render: function (dataItem, statuses) {
+//             $list.html(template(dataItem.content));
+//            }
+
+//        }
+//    });
 
 
-    // $('document').ready(function () {
-    //     $('#img-jplist').jplist({				
-    //         itemsBox: '#box-jplist', 
-    //         itemPath: '.img-box', 
-    //         panelPath: '.jplist-panel'
+// //   var date = $('.date_field');
+// //   date = date.substring(0,10);
+// //   $('.date_field').text(date);
 
-    //         //data source
-    //         , dataSource: {
+// });
 
-    //             type: 'server'
-    //             , server: {
+// $( document ).ajaxComplete(function() {
+//     $('.date_field').each(function () {
+//         var dateLong = $(this).text();
+//         date = dateLong.substring(0,10);
+//         $(this).text(date);
+//     });
 
-    //                 //jQuery ajax settings
-    //                 ajax: {
-    //                     url: '/api/images/'
-    //                     , dataType: 'json'
-    //                     , type: 'GET'
-    //                     , contentType: 'application/json'
-    //                 }
+//     $('.img-box .file_field').each(function(){
 
-    //             }
-    //             , render: function (dataItem, statuses) {
-    //                 $list.html(template(dataItem.content));
-    //             }
-    //         }
-    //     });
-    // });
+//         /*var fileName = fullPath.replace(/^.*[\\\/]/, '');
+//         $(this).text(fileName);*/
+
+//         var checker = $(this).closest('.img-box').find('.deleter');
+
+//         $(checker).attr('data-name', $('.file_name_field').text());
+
+//     });
+
+//     $('.img-box .complete_file').each(function(){
+//         var checker = $(this).closest('.img-box').find('.deleter');
+//         $(checker).val($(this).text())
+//     });
+
+//     $('.folder_field').each(function(){
+//         if ($(this).text()=='ambits') {
+//             $(this).text('\xC1mbitos')
+//         } else if ($(this).text()=='subjects'){
+//             $(this).text('Materias')
+//         }
+//     });
+
+
+//     $('.img-box').on('click', '.image', function(){
+//         var imageUrl = $('.file_field', this).text();
+//         $('#imgModal img').attr('src', imageUrl);
+//         $('#dloadBtn').attr('href', imageUrl);
+
+//         var theName = $('.file_name_field', this).text();
+//         $('#nombre').html('<strong>Nombre:</strong> '+theName);
+
+//         var theDate = $('.date_field', this).text();
+//         $('#fecha').html('<strong>Fecha:</strong> '+theDate);
+
+//         var attachedTo = $('.name_field', this).text();
+//         attachedTo = attachedTo.substring(0, attachedTo.indexOf('-'));
+//         $('#adjuntoa').html('<strong>Adjunto a:</strong> '+attachedTo);
+
+
+//     });
+
+
+//     $('#deleterBtn').on('mouseenter mouseleave', function () {
+
+//         var texts = [];
+//         var names = [];
+
+//         $(function () {
+//             $("input:checkbox[name=deleter]:checked").each(function(){
+//                 texts.push($(this).val());
+//                 names.push($(this).data('name'));
+//             });
+
+//             // alert(texts);
+//             $('#toBeDeleted').val(texts);
+//             $('#theNames').val(names);
+
+
+
+//             var str = '<ul>'
+
+//             names.forEach(function(slide) {
+//               str += '<li>'+ slide + '</li>';
+//             });
+
+//             str += '</ul>';
+//             document.getElementById("deleteList").innerHTML = str;
+//         });
+
+
+
+//     });
+
+//     $(function()
+//     {
+//       $('[name="deleter"]').change(function()
+//       {
+//         if ($(this).is(':checked')) {
+//            // Do something...
+//            $('#deleterBtn').prop("disabled", false);
+//         } else {
+//            $('#deleterBtn').prop("disabled", true) ;
+//         };
+//       });
+//     });
+
+
+
+// });
+
+
+
