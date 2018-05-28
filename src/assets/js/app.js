4e30dd4a62ebc861813a00de5d81744f1dfe2d36
Kinich Barcelo <kinich.barcelo@code-ing.com>
2018-05-28 16:16:16 -0500

    Create ODAs-list. Add last column to Materias-list
    Created brand new ODAs List. Discarded all previous code.
    New odas.html is now a Datatable.js table showing all the ODAs with their corresponging:
    Materia
    Bloque
    Tags
    # de Momenots
    Autor
    Fecha
    En EVA.
    Search is done on keyup and searches ALL fields.
    Also added the "En EVA' column to MAterias Lists.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -85,3 +85,14 @@ $('#tabla-materias').DataTable({
 });
 
 
+$('#tabla-odas').DataTable({
+    responsive: true,
+    "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+    },
+    // "columnDefs": [
+    //     { "orderable": false, "targets": [2] }
+    // ],
+
+});
+
